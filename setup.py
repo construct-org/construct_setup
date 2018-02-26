9ba4c9fcd844023165d82da9dfcb31cc1524311d
Dan Bradham <danielbradham@gmail.com>
2018-02-26 17:41:34 -0500

    Add construct_nuke

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ install_requires = [
     'construct_cli==0.0.2',
     'construct_launcher==0.0.1',
     'construct_maya==0.0.1',
+    'construct_nuke==0.0.1',
     'construct_templates==0.0.1',
     'fsfs==0.1.4',
 ]
@@ -17,6 +18,7 @@ dependency_links = [
     'git+ssh://git@github.com/construct-org/construct_cli.git@master#egg=construct_cli-0.0.2',
     'git+ssh://git@github.com/construct-org/construct_launcher.git@master#egg=construct_launcher-0.0.1',
     'git+ssh://git@github.com/construct-org/construct_maya.git@master#egg=construct_maya-0.0.1',
+    'git+ssh://git@github.com/construct-org/construct_nuke.git@master#egg=construct_nuke-0.0.1',
     'git+ssh://git@github.com/construct-org/construct_templates.git@master#egg=construct_templates-0.0.1',
     'git+ssh://git@github.com/danbradham/fsfs.git@master#egg=fsfs-0.1.4'
 ]
