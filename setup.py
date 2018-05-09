5ca8182a0665e07771f96b32bdfa612e27338cab
Dan Bradham <danielbradham@gmail.com>
2018-05-09 18:33:04 -0400

    0.1.18

--- setup.py
+++ setup.py
@@ -23,18 +23,18 @@ class Dependencies(object):
 
 
 requires = Dependencies()
-requires.git('construct-org', 'construct', '0.1.17')
+requires.git('construct-org', 'construct', '0.1.18')
 requires.git('construct-org', 'construct_cpenv', '0.1.1')
 requires.git('construct-org', 'construct_launcher', '0.1.5')
-requires.git('construct-org', 'construct_maya', '0.1.5')
-requires.git('construct-org', 'construct_nuke', '0.1.4')
+requires.git('construct-org', 'construct_maya', '0.1.6')
+requires.git('construct-org', 'construct_nuke', '0.1.5')
 requires.git('construct-org', 'construct_ui', '0.1.4')
-requires('fsfs==0.2.3')
+requires('fsfs==0.2.4')
 
 
 setup(
     name='construct_setup',
-    version='0.1.17',
+    version='0.1.18',
     author='Dan Bradham',
     author_email='danielbradham@gmail.com',
     description='Install construct core packages',
