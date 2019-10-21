fedd5a21ddfb1881805215042c5bd8099c48b0f2
Dan Bradham <danielbradham@gmail.com>
2019-10-21 17:25:44 -0400

    Update construct_nuke to 0.1.10

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires.git('construct-org', 'construct_cpenv', '0.1.1')
 requires.git('construct-org', 'construct_launcher', '0.1.5')
 requires.git('construct-org', 'construct_maya', '0.1.13')
 requires.git('construct-org', 'construct_hou', '0.1.1')
-requires.git('construct-org', 'construct_nuke', '0.1.9')
+requires.git('construct-org', 'construct_nuke', '0.1.10')
 requires.git('construct-org', 'construct_ui', '0.2.2')
 requires('fsfs')
 
